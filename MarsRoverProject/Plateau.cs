b76761a51f7ae2544a538c715039b60218522cad
Murat Erkan <muraterkan500@gmail.com>
2021-07-10 20:11:13 +0300

    Update Plateau.cs

--- MarsRoverProject/Plateau.cs
+++ MarsRoverProject/Plateau.cs
@@ -6,11 +6,10 @@ namespace MarsRoverProject
     {
         public static bool IsMoveAvailable(this PlateauModel plateau, PositionModel position)
         {
-            return plateau.MinWidth 
-                <= position.XPosition && position.XPosition 
-                <= plateau.Width && plateau.MinHeight 
-                <= position.YPosition && position.YPosition 
-                <= plateau.Height;
+            return plateau.MinWidth <= position.XPosition 
+                && position.XPosition <= plateau.Width 
+                && plateau.MinHeight <= position.YPosition 
+                && position.YPosition <= plateau.Height;
         }
     }
 }
